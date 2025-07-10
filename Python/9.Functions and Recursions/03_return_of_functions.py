5811a86c2ce7f234131492bc137e6aecb9a1e95a
Anas Norani <anasnorani47@gmail.com>
2025-07-10 08:07:34 -0700

    Fixed embedded repo issue

---
+++ Python/9.Functions and Recursions/03_return_of_functions.py
@@ -0,0 +1,12 @@
+#Return Value
+#function for addition
+def Add(a,b):
+    sum=a+b
+    return sum
+
+number1 =int(input('Enter number 01 :'))
+number2 =int(input('Enter number 02 :'))
+results =Add(number1,number2)
+print('The sum of numbers is',results)
+
+#return stores in results and than print results...easy
