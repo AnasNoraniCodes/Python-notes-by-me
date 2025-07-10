1dd445b08929d5057a7060cab499d30cca06e1d4
Anas Norani <anasnorani47@gmail.com>
2025-07-10 07:03:39 -0700

    ok

--- Python/9.Functions and Recursions/03_return_of_functions.py
+++ Python/9.Functions and Recursions/03_return_of_functions.py
@@ -1,5 +1,5 @@
 #Return Value
-
+#function for addition
 def Add(a,b):
     sum=a+b
     return sum
@@ -8,3 +8,5 @@ number1 =int(input('Enter number 01 :'))
 number2 =int(input('Enter number 02 :'))
 results =Add(number1,number2)
 print('The sum of numbers is',results)
+
+#return stores in results and than print results...easy
